3dc59845bf44ac519e7bf49457742b33fe6d3565
Ikhlas khan <ikhlas24khan@gmail.com>
2026-08-13 19:25:00 +0000

    Add JavaScript variables example

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const FirstName = "Ikhlas";
+const LastName = "Khan";
+let Age = 24;
+let AccountId = "ikhlas24khan"
+var AccountPassword = "PASSWORD"
+
+console.log(FirstName,LastName,Age,emailId)
+console.table([FirstName, LastName, Age, emailId]);
