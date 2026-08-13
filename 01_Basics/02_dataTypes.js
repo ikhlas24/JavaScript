82d7b616cf44fbb92b671152ff7ed0c2ac7a9f98
IkhlasPathan <ikhlas24pathan@gmail.com>
2026-08-13 19:59:06 +0000

    Added  02_dataTypes.js file

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+let Name = "Ikhlas";                         // String
+let Age = 24;                                // Number
+let Salary = 45000.50;                       // Number
+let IsLogin = false;                         // Boolean
+let Emp = null;                              // Null
+let contain;                                 // Undefined
+let BigNum = 12345678901234567890n;          // BigInt
+let ID = Symbol("EmployeeID");               // Symbol
+let OB = {"A": 1, "B": 2, "C": 3};           // Object
+
+console.log(typeof Name)     // => String
+console.log(typeof contain)  // => undefined
+console.log(typeof Emp)      // => null will show as object 
