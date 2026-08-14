358db15b7434e63fc4f0dda1986b8822540c50ff
IkhlasPathan <ikhlas24pathan@gmail.com>
2026-08-14 10:46:19 +0000

    new-branch creatd-to-tesst

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -3,6 +3,7 @@ const LastName = "Khan";
 let Age = 24;
 let AccountId = "ikhlas24khan"
 var AccountPassword = "PASSWORD"
+let New = "ABC"
 
 console.log(FirstName,LastName,Age,emailId)
 console.table([FirstName, LastName, Age, emailId]);
